031a7b254c97fa72928559716027b11efa2ff8d2
Juan Cotrino <juan.cotrino@outlook.com>
2022-05-23 15:21:17 +0200

    Initial Push

---
+++ droughts/lib.py
@@ -0,0 +1,24 @@
+import time
+
+def try_me():
+    time.sleep(2)
+    print('Is it working?')
+    time.sleep(2)
+    print('Not sure yet...')
+    time.sleep(3)
+    print('Ok, your computer will explode in...')
+    time.sleep(3)
+    print('3...')
+    time.sleep(1)
+    print('2...')
+    time.sleep(1)
+    print('1...')
+    time.sleep(1)
+    print('0.5...')
+    time.sleep(2)
+    print('No worries, the package works fine and no, your computer is not exploding...')
+    time.sleep(3)
+    print('... or is it?')
+
+if __name__ == "__main__":
+    try_me()
